7db6b271d18ae0cd16c7017e2ec0c13adbcc1f1a
Caroline <Caroline@LAPTOP-T9O2GLMC>
2023-10-15 16:07:51 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Circle
+    {
+        private double _Radius;  //konstruktor med parametern radie och en metod som returnerar arean
+        public Circle(double radius)
+        {
+            _Radius = radius;
+        }
+        public double getArea()  //metoden returnerar arean på cirkeln
+        {
+            return _Radius * _Radius * Math.PI;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(5);  //radien på första cirkeln är 5
+            Console.WriteLine("Arean på första cirkeln är: "+circle.getArea());  //kallar på metoden från Circle klassen
+
+            Circle circle2 = new Circle(6);  //radien på andra cirkeln är 6
+            Console.WriteLine("Arean på andra cirkeln är: "+circle2.getArea());  //kallar på metoden från Circle klassen
+        }
+    }
+}
